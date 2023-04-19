c5640f75ecbb6230959dceccf8d541eb8af74115
Samuel Guerra <56116583+SamG1002@users.noreply.github.com>
2023-04-19 11:31:36 -0300

    :fire: QUIZ

---
+++ questions.js
@@ -0,0 +1,108 @@
+export default [
+  {
+    question: "Onde nos pegamos pela primeira vez?",
+    answers: [
+      { option: "Seu Quarto", correct: true },
+      { option: "Meu Quarto", correct: false },
+      { option: "Ponto de Ônibus", correct: false },
+    ],
+    recompensa: "Você ganhou um bombom",
+    Prenda: "Você vai ter que me dar um bombom"
+  },
+  {
+    question: "Quais presentes eu dei para você no seu aniversário de 2022",
+    answers: [
+      { option: "Cesta, Coraline e Gon", correct: true },
+      { option: "Coraline e Cesta", correct: false },
+      { option: "Cesta e Coraline", correct: false },
+    ],
+    recompensa: "Ganhou um Abraço",
+    Prenda: "Vai receber cosquinha no suvaco"
+  },
+  {
+    question: "Qual meu Anime preferido?",
+    answers: [
+      { option: "Demons Slayes", correct: false },
+      { option: "Attack on TItans", correct: false },
+      { option: "FullMetal", correct: true },
+    ],
+    recompensa: "Ganhou um Selinho",
+    Prenda: "Samuel que vai escolher proxima coisa para assistirmos"
+  },
+  {
+    question: "Qual meu Jogo preferido",
+    answers: [
+      { option: "Cuphead", correct: false },
+      { option: "CS", correct: false },
+      { option: "God of War", correct: true },
+    ],
+    recompensa: "Nosso jogo preferido, eu fico mt feliz de vc ter gostado tb, merece um beijo de lingua",
+    Prenda: "É dificil saber, Vai tomar um Tapa na Raba"
+  },
+  {
+    question: "Qual a NOSSA Posição Preferida",
+    answers: [
+      { option: "Você por cima", correct: true },
+      { option: "De ladinho", correct: false },
+      { option: "4", correct: false },
+    ],
+    recompensa: "Pode dar um tapa na minha raba",
+    Prenda: "Vai receber cosquinha no umbigo"
+  },
+  {
+    question: "Qual a MINHA posição Preferida",
+    answers: [
+      { option: "Você por cima", correct: true },
+      { option: "De ladinho", correct: false },
+      { option: "papai mamae", correct: false },
+    ],
+    recompensa: "Pode dar 1 dedada",
+    Prenda: "Vai Tomar uma 1 dedada"
+  },
+  {
+    question: "Onde eu amo ser tocado por você?",
+    answers: [
+      { option: "Pescoço", correct: true },
+      { option: "Pau", correct: true },
+      { option: "Peito", correct: true },
+    ],
+    recompensa: "Todos eu gosto kkkkkk pode tocar aonde quiser em mim",
+    Prenda: ""
+  },
+  {
+    question: "Você me trocaria por... ?",
+    answers: [
+      { option: "Shrek 5", correct: false },
+      { option: "Jamais te trocaria", correct: true },
+      { option: "Viajar pro Japão", correct: false },
+      { option: "Conhecer o Yoshihiro Togashi", correct: false },
+    ],
+    recompensa: "Bom mesmo, pode pedir alguma coisa",
+    Prenda: "NOSSA VEI ADEUS"
+  },
+  {
+    question: "O que eu te trocaria ?",
+    answers: [
+      { option: "PC Gamer", correct: false },
+      { option: "Viver no país que quiser", correct: false },
+      { option: "São Paulo Campeão Mundial", correct: false },
+      { option: "Nada", correct: true },
+    ],
+    recompensa: "Nada nessa vida meu amor, nunca se esqueça",
+    Prenda: "Se valorize mais, você é tudo que eu já pedi nessa vida"
+  },
+ 
+  {
+    question: "Hoje é que dia ?",
+    answers: [
+      { option: "Dia que tomei a melhor decisão da minha vida", correct: true },
+      { option: "Dia que meu mundo começou a girar", correct: true },
+      { option: "Dia que meu mundo fez sentido", correct: true },
+      { option: "Dia que meu mundo se fez mais bonito", correct: true },
+      { option: "Dia que meu mundo se tornou o Seu", correct: true },
+      { option: "Dia que a melhor pessoa do mundo aceitou namorar comigo", correct: true },
+    ],
+    recompensa: "Todos corretos, tudo são meus sentimentos, eu te amo mais que tudo",
+    Prenda: ""
+  },
+];
---
+++ script.js
@@ -0,0 +1,78 @@
+const question = document.querySelector(".question");
+const answers = document.querySelector(".answers");
+const spnQtd = document.querySelector(".spnQtd");
+const textFinish = document.querySelector(".finish span");
+const content = document.querySelector(".content");
+const contentFinish = document.querySelector(".finish");
+const btnRestart = document.querySelector(".finish button");
+
+import questions from "./questions.js";
+
+let currentIndex = 0;
+let questionsCorrect = 0;
+
+btnRestart.onclick = () => {
+  form.style.display = "flex";
+  contentFinish.style.display = "none";
+
+  currentIndex = 0;
+  questionsCorrect = 0;
+  loadQuestion();
+};
+
+function nextQuestion(e) {
+  // ACERTAR
+  if (e.target.getAttribute("data-correct") === "true") {
+    questionsCorrect++;
+    Swal.fire({
+      icon: 'success',
+      title: 'Acertou! ',
+      text: questions[currentIndex].recompensa,
+    })
+  }else{
+    // ERRAR
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROU! ',
+      text: questions[currentIndex].Prenda,
+    })
+  }
+
+  if (currentIndex < questions.length - 1) {
+    currentIndex++;
+    loadQuestion();
+  } else {
+    finish();
+  }
+}
+
+function finish() {
+  textFinish.innerHTML = `você acertou ${questionsCorrect} de ${questions.length}`;
+  content.style.display = "none";
+  contentFinish.style.display = "flex";
+}
+
+function loadQuestion() {
+  spnQtd.innerHTML = `${currentIndex + 1}/${questions.length}`;
+  const item = questions[currentIndex];
+  answers.innerHTML = "";
+  question.innerHTML = item.question;
+
+  item.answers.forEach((answer) => {
+    const div = document.createElement("div");
+
+    div.innerHTML = `
+    <button class="answer" data-correct="${answer.correct}">
+      ${answer.option}
+    </button>
+    `;
+
+    answers.appendChild(div);
+  });
+
+  document.querySelectorAll(".answer").forEach((item) => {
+    item.addEventListener("click", nextQuestion);
+  });
+}
+
+loadQuestion();
